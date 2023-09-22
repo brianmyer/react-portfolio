2d73144a266b8c3fea4258b292325c052ebd395b
brianmyer <myer.brian@gmail.com>
2023-09-22 13:59:51 -0400

    starter code, nothing rendering yet

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// Bringing in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/NavTabs';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import App from './App.jsx';
+import AboutMe from './Pages/AboutMe';
+import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Resume from './Pages/Resume';
+import Error from './Pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
