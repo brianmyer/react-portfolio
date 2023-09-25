d816e40af1d0df8ad41947757045ae52549e023d
brianmyer <myer.brian@gmail.com>
2023-09-25 18:41:04 -0400

    added header component and placed navbar inside

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,14 @@
-import Nav from './components/NavTabs';
+import Nav from './NavTabs';
 
 
 export default function Header() {
     return (
-        <div className="border-top border-3 border-dark p-2 mb-2 border-opacity-50 text-center">
+        <div className="border-bottom border-3 border-dark p-2 mb-2 border-opacity-50 text-center">
             <a href="https://www.linkedin.com/in/brian-james-myer/">
 
-                <img src="../src/assets/LI-In-Bug.png"
-                    alt="LinkedIn logo"
-                    width="54"
-                    height="48"
-                />
+               Brian James Myer - Full Stack Web Developer
             </a>
-            <a href="https://github.com/brianmyer">
-
-                <img src="../src/assets/github-mark.png"
-                    alt="GitHub logo"
-                    width="48"
-                    height="48"
-                />
-            </a>
-            <a href="https://www.instagram.com/brianjamesmyer/">
-
-                <img src="../src/assets/ig-icon.png"
-                    alt="Instagram logo"
-                    width="48"
-                    height="48"
-                />
-            </a>
+            <Nav />
         </div>
     );
 }
