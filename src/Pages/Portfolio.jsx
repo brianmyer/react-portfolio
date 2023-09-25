f4e6b5e58bb40cfbecaae349fde71acdf6399e44
brianmyer <myer.brian@gmail.com>
2023-09-25 17:11:19 -0400

    added links to portfolio cards

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -2,26 +2,24 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-        <a href="#" className="btn">
-      <div className="card" style={{ width: "18rem" , height: "18rem"}}>
+      <div className="card" style={{ width: "18rem" }}>
           <img src="./src/assets/VW.png" className="card-img-top" alt="VoteWave logo" />
           <div className="card-body">
             <h5 className="card-title">VoteWave</h5>
             <p className="card-text">Your Voice, Your Choice.</p>
-
+            <button type="button" class="btn btn-dark" href="https://votewave-2770cf914d61.herokuapp.com/">Link to App</button>
+            <button type="button" class="btn btn-dark" href="https://github.com/brianmyer/votewave">GitHub Repo</button>
           </div>
       </div>
-        </a>
-        <a href="#" className="btn">
-      <div className="card" style={{ width: "18rem", height: "18rem"}}>
+      <div className="card" style={{ width: "18rem"}}>
           <img src="./src/assets/arvdark.png" className="card-img-top" alt="ARV logo" />
           <div className="card-body">
             <h5 className="card-title">ARV</h5>
             <p className="card-text">Know exactly where and when to pick up your loved ones.</p>
-
+            <button type="button" class="btn btn-dark" href="https://brianmyer.github.io/ARV/">Link to App</button>
+            <button type="button" class="btn btn-dark" href="https://github.com/brianmyer/ARV">GitHub Repo</button>
           </div>
       </div>
-        </a>
 
     </div>
   );
