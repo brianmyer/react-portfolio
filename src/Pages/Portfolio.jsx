2dd420d5330fc480ef9e306c0e3361f747948889
brianmyer <myer.brian@gmail.com>
2023-09-25 16:58:40 -0400

    images rendering, found correct src location

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -2,26 +2,26 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <div className="card" style={{ width: "18rem" , height: "18rem"}}>
         <a href="#" className="btn">
-          <img src="..." className="card-img-top" alt="..." />
+      <div className="card" style={{ width: "18rem" , height: "18rem"}}>
+          <img src="./src/assets/VW.png" className="card-img-top" alt="VoteWave logo" />
           <div className="card-body">
             <h5 className="card-title">VoteWave</h5>
-            <p className="card-text">Your Voice, Your Choice</p>
+            <p className="card-text">Your Voice, Your Choice.</p>
 
           </div>
-        </a>
       </div>
-      <div className="card" style={{ width: "18rem", height: "18rem"}}>
+        </a>
         <a href="#" className="btn">
-          <img src="../assets/arvgray.png" className="card-img-top" alt="..." />
+      <div className="card" style={{ width: "18rem", height: "18rem"}}>
+          <img src="./src/assets/arvdark.png" className="card-img-top" alt="ARV logo" />
           <div className="card-body">
             <h5 className="card-title">ARV</h5>
             <p className="card-text">Know exactly where and when to pick up your loved ones.</p>
 
           </div>
-        </a>
       </div>
+        </a>
 
     </div>
   );
